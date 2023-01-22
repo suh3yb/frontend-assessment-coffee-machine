8f56fbdd615e963d8ae6abf5e3629884086bc0a1
Suheyb Coskun <suheyb.c@gmail.com>
2023-01-22 17:39:03 +0100

    update types

--- src/types.ts
+++ src/types.ts
@@ -1,36 +1,61 @@
+export interface CoffeeType {
+  _id: string;
+  name: string;
+  sizes: string[];
+  extras: string[];
+}
+
+export interface Size {
+  _id: string;
+  name: string;
+  __v?: number;
+}
+
+export interface Extra {
+  _id: string;
+  name: string;
+  subselections: {
+    _id: string;
+    name: string;
+  }[];
+}
+
 export interface CoffeeDataResponse {
   _id: string;
-  types: {
-    _id: string;
+  types: CoffeeType[];
+  sizes: Size[];
+  extras: Extra[];
+}
+
+export interface SelectedExtras {
+  [extraId: string]: {
     name: string;
-    sizes: string[];
-    extras: string[];
-  }[];
-  sizes: {
-    _id: string;
-    name: string;
-    __v?: number;
-  }[];
-  extras: {
-    _id: string;
-    name: string;
-    subselections: {
-      _id: string;
-      name: string;
-    }[];
-  }[];
+    subselection: { id: string; name: string };
+  };
 }
 
 export interface State {
+  coffeeData: CoffeeDataResponse | undefined;
   activeStepIndex: number;
-  selectedType: string | null;
-  selectedSize: string | null;
-  selectedExtras: string[] | [];
+  selectedType: { id: string; name: string } | null;
+  availableSizes: Size[] | null;
+  availableExtras: Extra[] | null;
+  selectedSize: { id: string; name: string } | null;
+  selectedExtras: SelectedExtras;
 }
 
 export type Action =
+  | { type: 'setCoffeeData'; payload: CoffeeDataResponse }
   | { type: 'setActiveStepIndex'; payload: number }
-  | { type: 'setType'; payload: string }
-  | { type: 'setSize'; payload: string }
-  | { type: 'addExtra'; payload: string }
+  | { type: 'setType'; payload: { id: string; name: string } }
+  | { type: 'setAvailableSizes'; payload: Size[] }
+  | { type: 'setAvailableExtras'; payload: Extra[] }
+  | { type: 'setSize'; payload: { id: string; name: string } }
+  | {
+      type: 'addExtra';
+      payload: {
+        extra: { id: string; name: string };
+        subselection: { id: string; name: string };
+      };
+    }
   | { type: 'reset' };
