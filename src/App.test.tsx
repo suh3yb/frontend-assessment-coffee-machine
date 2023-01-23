1e4f501bc289ee1b08602d0775e2780b69a157ad
Suheyb Coskun <suheyb.c@gmail.com>
2023-01-23 16:24:27 +0100

    update App.test.tsx

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
-import { render, screen, waitFor } from '@testing-library/react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
 jest.setTimeout(10000);
+
+const renderAndWaitForCoffeeTypes = async () => {
+  render(<App />);
+  await waitFor(
+    () => {
+      expect(screen.queryByText('Dark Roasted Beans')).not.toBeInTheDocument();
+    },
+    { timeout: 10000 }
+  );
+  await waitFor(() => {
+    expect(screen.getByText('Brew with Lex')).toBeInTheDocument();
+  });
+};
 
 describe('App', () => {
   it('renders landing page initially => title, subtitle, link', () => {
@@ -17,23 +30,56 @@ describe('App', () => {
   });
 
   it('renders coffee types when data is fetched', async () => {
-    render(<App />);
-    await waitFor(
-      () => {
-        expect(
-          screen.queryByText('Dark Roasted Beans')
-        ).not.toBeInTheDocument();
-      },
-      { timeout: 10000 }
-    );
-    await waitFor(() => {
-      expect(screen.getByText('Brew with Lex')).toBeInTheDocument();
-    });
+    await renderAndWaitForCoffeeTypes();
 
     expect(screen.getByText('Brew with Lex')).toBeInTheDocument();
     expect(screen.getByText('Select your style')).toBeInTheDocument();
     expect(screen.getByText('Ristretto')).toBeInTheDocument();
     expect(screen.getByText('Espresso')).toBeInTheDocument();
     expect(screen.getByText('Cappuccino')).toBeInTheDocument();
+  });
+
+  it('renders coffee size and extras when a type is selected', async () => {
+    await renderAndWaitForCoffeeTypes();
+
+    fireEvent.click(screen.getByText('Ristretto'));
+    await waitFor(() => {
+      expect(screen.getByText('Large')).toBeInTheDocument();
+    });
+    expect(screen.getByText('Select your size')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Large'));
+    await waitFor(() => {
+      expect(
+        screen.getByText('Select the amount of sugar')
+      ).toBeInTheDocument();
+    });
+  });
+
+  it('renders correct overview after all selections', async () => {
+    await renderAndWaitForCoffeeTypes();
+
+    fireEvent.click(screen.getByText('Ristretto'));
+    await waitFor(() => {
+      expect(screen.getByText('Large')).toBeInTheDocument();
+    });
+    expect(screen.getByText('Select your size')).toBeInTheDocument();
+
+    fireEvent.click(screen.getByText('Large'));
+    await waitFor(() => {
+      expect(
+        screen.getByText('Select the amount of sugar')
+      ).toBeInTheDocument();
+    });
+
+    fireEvent.click(screen.getByText('Select the amount of sugar'));
+    await waitFor(() => {
+      expect(screen.getByText('Normal')).toBeInTheDocument();
+    });
+
+    fireEvent.click(screen.getByText('Normal'));
+    await waitFor(() => {
+      expect(screen.getByText('Overview')).toBeInTheDocument();
+    });
   });
 });
